4a9f97e2fa47b3a02d05719c02ec1abccde740ce
백종현 <bhyunvin@naver.com>
2020-02-08 22:58:44 +0900

    Add files via upload

---
+++ Programming/OKJavaGoInHome.java
@@ -0,0 +1,24 @@
+package Programming;
+
+import Programming.org.opentutorials.iot.*;
+
+public class OKJavaGoInHome {
+	public static void main(String[] args) {
+		String id = "JAVA APT 507";
+		
+		//Elevator call
+		Elevator myElevator = new Elevator(id);
+		myElevator.callForUp(1);
+		
+		//Security off
+		Security mySecurity = new Security(id);
+		mySecurity.off();
+		
+		//Light on
+		Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting(id+" / floor Lamp");
+		floorLamp.on();
+	}
+}
